ded6b1b6dcce61cbc67d57276c7443da6fa00bdb
pnw-ology <brian.johnson.23@icloud.com>
2018-01-09 13:02:15 -0800

    Update location-bar.js

--- location-bar.js
+++ location-bar.js
@@ -11,8 +11,8 @@
 // new wrapper: START
 
 !(function (name, context, definition) {
-  if (typeof exports == 'object') { module.exports = definition(require); } else if (typeof define == 'function' && define.amd) { define(definition); } else if (typeof YUI == "function") { YUI.add(name, definition, '@VERSION@', {requires: []}); } else { context[name] = definition(); }
-}).call(this, 'History', this, function (require) {
+  if (typeof exports == 'object') { module.exports = definition(); } else if (typeof define == 'function' && define.amd) { define(definition); } else if (typeof YUI == "function") { YUI.add(name, definition, '@VERSION@', {requires: []}); } else { context[name] = definition(); }
+}).call(this, 'History', this, function () {
   'use strict';
 
 // new wrapper: END
